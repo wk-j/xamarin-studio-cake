723418a7e7a8b4980f7a1b36f05cabab7eded02d
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-08-05 23:34:10 +0700

    Initial commit

---
+++ XamarinStudio.Cake/Core/CakeTreeViewPad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using MonoDevelop.Ide.Gui.Components;
+using MonoDevelop.Ide.Gui.Pads;
+using MonoDevelop.Projects;
+
+namespace XamarinStudio.Cake.Core {
+	public class CakeTreeViewPad : TreeViewPad {
+		public CakeTreeViewPad() {
+		}
+
+		public override void Initialize(NodeBuilder[] builders, TreePadOption[] options, string menuPath) {
+			base.Initialize(builders, options, menuPath);
+
+			var range = Enumerable.Range(0, 10).ToList();
+			range.ForEach(x => {
+				var it = new TreeViewItem($"Hello {x + 1}", Gtk.Stock.New);
+				treeView.AddChild(it);
+			});
+		}
+		protected virtual void OnOpenWorkspace(object sender, WorkspaceItemEventArgs e) {
+
+		}
+
+		protected virtual void OnCloseWorkspace(object sender, WorkspaceItemEventArgs e) {
+
+		}
+	}
+}
---
+++ XamarinStudio.Cake/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"XamarinStudio.Cake",
+	Namespace = "XamarinStudio.Cake",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("XamarinStudio.Cake")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("XamarinStudio.Cake")]
+[assembly: AddinAuthor("wk")]
